0cd5664ad50e4771b1845ac0dfc0456d0edc84fc
Thiago Dias <111087294+ThiagoBDias@users.noreply.github.com>
2026-05-02 18:17:37 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // 1. Configura o Banco de Dados ANTES de construir o app
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // 2. Adiciona os controladores (os garçons)
 builder.Services.AddControllers();
