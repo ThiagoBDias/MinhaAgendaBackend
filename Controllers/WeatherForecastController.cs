251c9bf45a9d6bd12da5f8bb43256835608f0077
Thiago Dias <111087294+ThiagoBDias@users.noreply.github.com>
2026-05-05 11:39:25 -0300

    Update WeatherForecastController.cs

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -20,6 +20,5 @@ public class WeatherForecastController : ControllerBase
             TemperatureC = Random.Shared.Next(-20, 55),
             Summary = Summaries[Random.Shared.Next(Summaries.Length)]
         })
-        .ToArray();
     }
 }
